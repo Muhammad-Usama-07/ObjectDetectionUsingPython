bbb902a233a524871923d3256e95cd384772e829
Muhammad Usama <muhammadusama2k17@gmail.com>
2021-07-31 16:17:15 +0500

    Displaying image

--- FaceRecognition/Work.py
+++ FaceRecognition/Work.py
@@ -4,3 +4,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# Displaying image
+img = cv2.imread('images/RDJ1.jpg')
+cv2.imshow('imgage',img)
+
+# Not to close image automatically
+cv2.waitKey(0)
+
