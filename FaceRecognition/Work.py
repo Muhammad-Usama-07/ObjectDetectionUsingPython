6e22b07f6de6cfa8da7a5860717cdf95b18cacce
Muhammad Usama <muhammadusama2k17@gmail.com>
2021-07-31 17:26:07 +0500

    adding haarcascade_frontalface_default file

--- FaceRecognition/Work.py
+++ FaceRecognition/Work.py
@@ -6,8 +6,18 @@ from matplotlib import pyplot as plt
 
 # Displaying image
 img = cv2.imread('images/RDJ1.jpg')
-cv2.imshow('imgage',img)
 
+# Applying bounding box
+face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converting to gray scale
+faces = face_casecade.detectMultiScale(gray,1.2, 6)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow('img',img)
 # Not to close image automatically
 cv2.waitKey(0)
+
+
 
